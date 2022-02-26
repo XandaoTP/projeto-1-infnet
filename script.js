0fa1242b65155cc12411c9ebd512a1b2af4b5e69
unknown <alexandre8603@gmail.com>
2022-02-26 10:02:25 -0300

    alteracao css e html

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ document.getElementById("botao").onclick = function() { abrir ()}
 function abrir() {
 document.getElementById("info").classList.toggle("show");
 }
+
+function openSideBar() {
+    const cartSidebarEl = document.querySelector('.cart-sidebar')
+    cartSidebarEl.classList.add('cart-sidebar-open')
+    }
+    function closeSideBar() {
+        const cartSidebarEl = document.querySelector('.cart-sidebar')
+        cartSidebarEl.classList.remove('cart-sidebar-open')
+    }
