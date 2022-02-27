9f583df93bbb9072960557908dad726bb4c98aca
unknown <alexandre8603@gmail.com>
2022-02-27 12:05:17 -0300

    carrinho

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
-document.getElementById("botao").onclick = function() { abrir ()}
-
-function abrir() {
-document.getElementById("info").classList.toggle("show");
+const CartwindowEl = document.querySelector('.cartwindow')
+function openWindowCart () {
+    CartwindowEl.classList.add("cartwindowopen")
 }
-
-function openSideBar() {
-    const cartSidebarEl = document.querySelector('.cart-sidebar')
-    cartSidebarEl.classList.add('cart-sidebar-open')
-    }
-    function closeSideBar() {
-        const cartSidebarEl = document.querySelector('.cart-sidebar')
-        cartSidebarEl.classList.remove('cart-sidebar-open')
-    }
+function closeWindowCart () {
+    CartwindowEl.classList.remove('cartwindowopen')
+}
+const btnCartEl = document.getElementById('buttoncart')
+    btnCartEl.addEventListener('click', openWindowCart)
+const closeCartEl = document.getElementById('btnclosecart')
+    closeCartEl.addEventListener('click', closeWindowCart )
+    
